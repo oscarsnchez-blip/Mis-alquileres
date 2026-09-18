d10eee96757d11d26ce0e87c84a11685c4d27b90
oscarsnchez-blip <oscarsnchez@gmail.com>
2026-09-18 17:04:25 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='mininos-flat-v1';
-const ASSETS=['./','./index.html','./app.css','./app.js'];
+const ASSETS=['./','./index.html','./app.css','./app.js','./app-rentabilidad-fechas.js'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
